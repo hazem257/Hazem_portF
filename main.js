ee0c4cd39d655e9a153474a64a843e2aece08e72
hazem257 <161766768+hazem257@users.noreply.github.com>
2024-08-27 12:38:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ function scrollActive() {
 let re4 = document.getElementById('mylink');
 let re = document.getElementById('dar');
 let origin = 'style.css';
-let newHref = 'style2.css';
+let newHref = 'dark.css';
 let isorigin = true;
 re.addEventListener('click',()=>{
   if(isorigin){
